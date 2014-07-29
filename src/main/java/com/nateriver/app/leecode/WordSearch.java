648ad6a9c1e6994e96ae5b5ea6fd5a381a4b6097
nateriver520 <469216661@qq.com>
2014-07-29 21:41:53 +0800

    update word search

--- src/main/java/com/nateriver/app/leecode/WordSearch.java
+++ src/main/java/com/nateriver/app/leecode/WordSearch.java
@@ -1,5 +1,8 @@
 package com.nateriver.app.leecode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
      Word Search
      Given a 2D board and a word, find if the word exists in the grid.
@@ -21,12 +24,43 @@ package com.nateriver.app.leecode;
 
 
 public class WordSearch {
-    public static boolean exist(int[][] board, String word){
-        //todo: to be finished
-        return true;
+    public static boolean exist(char[][] board, int i, int j,  String word, int offset, Map<String,Integer> stepHash ){
+        if(offset == word.length()) return true;
+        if(i < 0 || j <0 || i > board.length - 1 || j > board[i].length -1|| stepHash.containsKey(i + "" + j) || board[i][j] != word.toCharArray()[offset])
+            return false;
+
+        stepHash.put(i + "" + j, 1);
+        if(exist(board,i -1,j,word,offset + 1, stepHash) || exist(board,i + 1,j,word,offset + 1, stepHash) ||exist(board,i,j -1,word,offset + 1, stepHash) ||exist(board, i ,j + 1 ,word,offset + 1, stepHash) )
+            return true;
+        stepHash.remove(i + "" + j);
+        return false;
+    }
+
+    public static boolean search(char[][] board, String word){
+        int n = board.length;
+        int m = board[0].length;
+
+        for(int i =0; i < n; i++){
+            for(int j=0; j < m; j++){
+                if(board[i][j] == word.toCharArray()[0]){
+                    Map<String,Integer> ways = new HashMap<>();
+                    if(exist(board, i,j, word, 0, ways))
+                        return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
-        System.out.println("123");
+        char[][] board = {
+                {'A','B','C','E'},
+                {'S', 'F', 'C', 'S'},
+                {'A','D','E','E'},
+        };
+        System.out.println(search(board,"ABCCED"));
+        System.out.println(search(board,"SEE"));
+        System.out.println(search(board,"ABCB"));
     }
 }
