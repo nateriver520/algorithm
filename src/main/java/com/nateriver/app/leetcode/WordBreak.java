16f861c8bed5714e3cfef71b9ead084cc1b425b1
nateriver520 <469216661@qq.com>
2014-08-19 21:57:31 +0800

    [LeetCode] Word Break (Add none time exceed version )

--- src/main/java/com/nateriver/app/leetcode/WordBreak.java
+++ src/main/java/com/nateriver/app/leetcode/WordBreak.java
@@ -34,12 +34,34 @@ public class WordBreak {
         return false;
     }
 
+    public boolean wordBreak1(String s, Set<String> dict){
+        if(s.length() == 0) {
+            return false;
+        }
+        boolean[] canBreak = new boolean[s.length()+1]; // canBreak[i]: whether s[0,i) can be break
+        canBreak[0] = true;     // base case
+
+        for(int i=1; i<=s.length(); i++) {  // last case: s[0,s.length()) == s
+            boolean flag = false;
+            for(int j=0; j<i; j++) {    // j should be smaller than i to include at least character
+                // s[0,i) can be break only when s[0,j) can be break AND s[j,i) is in the dict
+                if(canBreak[j] && dict.contains(s.substring(j, i))) {
+                    flag = true;
+                    break;
+                }
+            }
+            canBreak[i] = flag;
+        }
+
+        return canBreak[s.length()];
+    }
+
     public static void main(String[] args) {
         Set<String> dict = new HashSet<>();
         dict.add("leet");
         dict.add("code");
 
         WordBreak wb = new WordBreak();
-        System.out.println(wb.wordBreak("leetcode",dict));
+        System.out.println(wb.wordBreak1("leetcode",dict));
     }
 }
