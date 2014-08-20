5e8cb47837bf751fa94d61a7baba655d149600bf
nateriver520 <469216661@qq.com>
2014-08-20 20:17:31 +0800

    [LeetCode] Word Ladder (Accepted Version)

--- src/main/java/com/nateriver/app/leetcode/WordLadder.java
+++ src/main/java/com/nateriver/app/leetcode/WordLadder.java
@@ -33,30 +33,51 @@ public class WordLadder {
         }
     }
 
-    Map<String, Integer> visited = new HashMap<>();
+
+
     public int ladderLength(String start, String end, Set<String> dict) {
-        Queue<Word> q = new ArrayDeque<>();
-        q.add(new Word(start,1));
-        while (!q.isEmpty()){
-            Word curWord = q.poll();
-            visited.put(curWord.value,1);
-            char[] chars = curWord.value.toCharArray();
-            for(int i = 0; i < chars.length; i++){
-                char orgC = chars[i];
-                for (char c = 'a'; c <= 'z'; ++c) {
-                    chars[i] = c;
-                    String judge = String.valueOf(chars);
-                    if(judge.equals(end)){
-                        return curWord.level + 1;
-                    }
-                    if(dict.contains(judge) && !visited.containsKey(judge)){
-                        q.add(new Word(judge,curWord.level+1));
-                    }
-                    chars[i] = orgC;
-                }
-            }
+        Map<String, Integer> startVisited = new HashMap<>();
+        Map<String, Integer> endVisited = new HashMap<>();
+        Queue<Word> startQ = new ArrayDeque<>();
+        Queue<Word> endQ = new ArrayDeque<>();
+
+        dict.remove(start);
+        startQ.add(new Word(start, 0));
+        endQ.add(new Word(end,0));
+        endVisited.put(end, 0);
+
+        while (!startQ.isEmpty() && !endQ.isEmpty()){
+            Word startWord = startQ.poll();
+            Word endWord = endQ.poll();
+
+            int res = handleSingleWord(startWord, startVisited,startQ,endVisited,dict);
+            if(res > 0) return res;
+
+            res =   handleSingleWord(endWord, endVisited,endQ,startVisited,dict);
+            if(res > 0) return res;
+
         }
         return 0;
+    }
+
+    public int handleSingleWord(Word word, Map<String, Integer> orgMap,Queue<Word> orgQ, Map<String, Integer> diffMap, Set<String> dict){
+        char[] chars = word.value.toCharArray();
+        for(int i = 0; i < chars.length; i++){
+            char orgC = chars[i];
+            for (char c = 'a'; c <= 'z'; ++c) {
+                chars[i] = c;
+                String judge = String.valueOf(chars);
+                if(diffMap.containsKey(judge)){
+                    return word.level + 1  + diffMap.get(judge) + 1;
+                }
+                if(dict.contains(judge) && !orgMap.containsKey(judge)){
+                    orgMap.put(judge, word.level + 1);
+                    orgQ.add(new Word(judge, word.level +1));
+                }
+                chars[i] = orgC;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -68,7 +89,21 @@ public class WordLadder {
         dic.add("dog");
         dic.add("lot");
         dic.add("log");
-        System.out.println(ladder.ladderLength("hit", "cog", dic));
+        System.out.println(ladder.ladderLength("hit", "cog", dic)); //5
+
+        dic.clear();
+        dic.add("a");
+        dic.add("b");
+        dic.add("c");
+        System.out.println(ladder.ladderLength("a", "c", dic)); //2
+
+
+
+        dic.clear();
+        dic.add("hot");
+        dic.add("dog");
+        dic.add("dot");
+        System.out.println(ladder.ladderLength("hot", "dog", dic)); //3
     }
 
 }
