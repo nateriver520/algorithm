38f8ff09ffbf125cca2974029854fec4097567b1
nateriver520 <469216661@qq.com>
2014-09-13 17:23:39 +0800

    [LeetCode] Copy List with Random Pointer

--- src/main/java/com/nateriver/app/leetcode/CopyListwithRandomPointer.java
+++ src/main/java/com/nateriver/app/leetcode/CopyListwithRandomPointer.java
@@ -2,9 +2,6 @@ package com.nateriver.app.leetcode;
 
 import com.nateriver.app.models.RandomListNode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * https://oj.leetcode.com/problems/copy-list-with-random-pointer/
  A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
@@ -12,15 +9,44 @@ import java.util.Map;
  Return a deep copy of the list.
  */
 public class CopyListwithRandomPointer {
-    private Map<Integer,RandomListNode> _copyHash= new HashMap<>();
+
+    /**
+     *  eg : copy 2 -> 3 -> 4
+     *  step 1: 2 -> 2 -> 3 -> 3 -> 4 -> 4
+     *  step 2: copy random
+     */
     public RandomListNode copyRandomList(RandomListNode head) {
         if(head == null) return null;
-        if(_copyHash.containsKey(head.hashCode())) return _copyHash.get(head.hashCode());
+        RandomListNode copyHead;
+        RandomListNode cur = head;
 
-        RandomListNode copyHead = new RandomListNode(head.label);
-        copyHead.next = copyRandomList(head.next);
-        copyHead.random = copyRandomList(head.next);
-        _copyHash.put(copyHead.hashCode(),copyHead);
+        //copy node
+        while (cur != null){
+            RandomListNode copy = new RandomListNode(cur.label);
+            copy.next = cur.next;
+            cur.next = copy;
+            cur = copy.next;
+        }
+        copyHead = head.next;
+        cur = head;
+
+        //copy random link
+        while (cur != null){
+            if(cur.random != null)
+                cur.next.random = cur.random.next;
+            cur = cur.next.next;
+        }
+
+        cur = head;
+        //split
+        while (cur != null){
+            RandomListNode copy = cur.next;
+            cur.next = copy.next;
+            if(copy.next != null)
+                copy.next = copy.next.next;
+            cur = cur.next;
+        }
+
         return copyHead;
     }
 }
