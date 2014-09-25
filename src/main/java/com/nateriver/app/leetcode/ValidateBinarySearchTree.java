1795c268bc782be6f121098a2e403680c44896c2
nateriver520 <469216661@qq.com>
2014-09-25 23:35:14 +0800

    [LeetCode] Validate Binary Search Tree

---
+++ src/main/java/com/nateriver/app/leetcode/ValidateBinarySearchTree.java
@@ -0,0 +1,31 @@
+package com.nateriver.app.leetcode;
+
+import java.util.Stack;
+
+/**
+ Given a binary tree, determine if it is a valid binary search tree (BST).
+
+ Assume a BST is defined as follows:
+
+ The left subtree of a node contains only nodes with keys less than the node's key.
+ The right subtree of a node contains only nodes with keys greater than the node's key.
+ Both the left and right subtrees must also be binary search trees.
+ */
+public class ValidateBinarySearchTree {
+    Stack<Integer> _stack = new Stack<>();
+
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        boolean res = isValidBST(root.left);
+        if(!res || !_stack.isEmpty() && root.val <= _stack.peek()) return false;
+        _stack.push(root.val);
+        return isValidBST(root.right);
+    }
+
+    public static void main(String[] args) {
+        ValidateBinarySearchTree vt = new ValidateBinarySearchTree();
+        TreeNode root = new TreeNode(0);
+        root.left = new TreeNode(-1);
+        System.out.println(vt.isValidBST(root));
+    }
+}
