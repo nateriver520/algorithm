34f2de53e8de4403a5cbd457a62c7cb8b8bcbbf4
nateriver520 <469216661@qq.com>
2014-09-27 23:12:16 +0800

    [leet code] Best Time to Buy and Sell Stock II

---
+++ src/main/java/com/nateriver/app/leetcode/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,24 @@
+package com.nateriver.app.leetcode;
+
+/**
+ Say you have an array for which the ith element is the price of a given stock on day i.
+
+ Design an algorithm to find the maximum profit. You may complete as many transactions as you like
+ (ie, buy one and sell one share of the stock multiple times).
+ However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ */
+public class BestTimetoBuyandSellStockII {
+    public int maxProfit(int[] prices) {
+        int size = prices.length;
+        if(size < 2)
+            return 0;
+        int profit = 0;
+        for(int i =0; i< size-1; i++){
+            if(prices[i] < prices[i+1]){
+                profit += prices[i+1] - prices[i];
+            }
+        }
+
+        return profit;
+    }
+}
